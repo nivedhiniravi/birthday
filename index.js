07df7a6552f62c439f3266e06b29e7326fba626b
Nivedhini <nivethiniravi@gmail.com>
2021-08-14 23:24:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$('document').ready(function(){
+	$('#turn_on').click(function(){
+        $("body"). addClass("active");    
+    });
+
+    $('#balid').click(function(){
+        $("#bal img"). addClass("balloon");
+    });
+});
